6ff7e88a401f7d2f970c5440523b79ae6e49854f
Sidney Borne <sidneys1@live.com>
2018-06-18 17:52:56 -0400

    operands

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,7 +1,110 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace Core {
+    public class Operand {
+        public enum OperandType : byte {
+            Register = 0x00, // 00
+            DereferencedRegister = 0x40, // 01
+            IndexedRegister = 0x80, // 10
+            Constant = 0xC0 // 11
+        }
+
+        public enum RegisterType : byte {
+            A,
+            B,
+            C, 
+            D,
+            X,
+            Y,
+            Z,
+            I,
+            J
+        }
+
+        const byte REGISTER_MASK = 0x3F;
+
+        public OperandType OpType { get; }
+        public RegisterType Register { get; }
+        public Operand Index { get; }
+        public UInt32 Value { get; }
+
+        public Operand(Stream input) {
+            var buffer = new byte[4];
+            var operand_span = buffer.AsSpan(0, 1);
+            var const_span = buffer.AsSpan();
+            input.Read(operand_span);
+
+            OpType = (OperandType)(buffer[0] & REGISTER_MASK);
+
+            switch (OpType) {
+                case OperandType.IndexedRegister:
+                    Index = new Operand(input);
+                    goto case OperandType.Register; // Fallthrough
+
+                case OperandType.Register:
+                case OperandType.DereferencedRegister:
+                    Register = (RegisterType)(buffer[0] & ~REGISTER_MASK);
+                    break;
+
+                case OperandType.Constant:
+                    input.Read(const_span);
+                    Value = BitConverter.ToUInt32(const_span);
+                    break;
+            }
+        }
+
+        public override string ToString() {
+            switch (OpType) {
+                case OperandType.Register:
+                    return Register.ToString();
+                case OperandType.DereferencedRegister:
+                    return $"*{Register}";
+                case OperandType.IndexedRegister:
+                    return $"{Register}[{Index}]";
+                case OperandType.Constant:
+                    return $"0x{Value:0X}";
+            }
+            return "ERROR";
+        }
+    }
+
+    public class Instruction {
+        public Opcodes.Opcode Opcode { get; }
+
+        public Operand[] Operands { get; }
+
+        public int OperandCount { get; }
+
+        public Instruction(Stream input) {
+            var read_buffer = new byte[1];
+            var opcode_span = read_buffer.AsSpan(0, 1);
+            input.Read(opcode_span);
+            Opcode = (Opcodes.Opcode)read_buffer[0];
+
+            OperandCount = Opcodes.OpcodeLength[Opcode];
+            Operands = new Operand[OperandCount];
+
+            for (int i = 0; i < OperandCount; i++) {
+                Operands[i] = new Operand(input);
+            }
+        }
+
+        public override string ToString() {
+            var builder = new StringBuilder();
+
+            builder.Append(Opcode);
+
+            foreach (var operand in Operands) {
+                builder.Append(" ");
+                builder.Append(operand);
+            }
+
+            return builder.ToString();
+        }
+    }
+
     public static class Opcodes {
         public enum Opcode : byte {
             Nop,
@@ -22,7 +125,7 @@ namespace Core {
     }
 
     class Program {
-        const byte REGISTER_MASK = 0x3F;
+        
         const int MEM_SIZE = 1024;
         static readonly byte[] Memory = new byte[MEM_SIZE];
 
