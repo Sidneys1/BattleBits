7ea3f3e1596c4ab249663646bf6576dda5db0bc0
Sidney Borne <sidneys1@live.com>
2018-06-18 17:05:58 -0400

    initial commit

---
+++ Core/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Core {
+    public static class Opcodes {
+        public enum Opcode : byte {
+            Nop,
+            ADD,
+            SUB,
+            MUL,
+            DIV,
+            JEQ,
+            JNE,
+            JGT,
+            JMP
+        }
+
+        public static System.Collections.Generic.Dictionary<Opcode, int> OpcodeLength = new System.Collections.Generic.Dictionary<Opcode, int> {
+            {Opcode.Nop, 0},
+            {Opcode.ADD, 3}
+        };
+    }
+
+    class Program {
+        const byte REGISTER_MASK = 0x3F;
+        const int MEM_SIZE = 1024;
+        static readonly byte[] Memory = new byte[MEM_SIZE];
+
+        static MemoryStream memStream = new MemoryStream(Memory);
+
+        static void Main(string[] args) {
+            byte[] opcode = new byte[1];
+            memStream.Read(opcode.AsSpan());
+            Console.WriteLine($"Hello World! Memory[0] is {(Opcodes.Opcode)opcode[0]}");
+            Memory[0] = 1;
+            memStream.Seek(0, SeekOrigin.Begin);
+            memStream.Read(opcode.AsSpan());
+            Console.WriteLine($"Hello World! Memory[0] is {(Opcodes.Opcode)opcode[0]}");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,37 @@
+OP + REG + REG + REG
+OP + REG + REG + EXTRA
+OP + REG + 2xEXTRA
+OP + 3xEXTRA
+
+
+Registers:
+
+A B C - X Y Z - I J
+
+Also O(verflow), N(ull), IP
+
+Operands:
+(&)Register([]), const
+
+Three operands:
+
+ADD A B C
+SUB A B C
+MUL A B C
+DIV A B C
+
+Two operands
+
+JEQ A B + extra
+JNE A B + extra
+JGT A B + extra
+
+
+One operand:
+
+
+Zero Operands:
+
+INT + extra??
+NOP
+JMP (Unconditional) + extra
